ee6b7659845da07eafbcb7b991a59aad5b656314
Cavin Stone <cstone2003@hotmail.com>
2013-12-26 22:02:32 -0600

    Add Carrierwave To Show Form

--- app/uploaders/page_image_uploader.rb
+++ app/uploaders/page_image_uploader.rb
@@ -1,26 +1,19 @@
 # encoding: utf-8
 
-class ShowGuestImageUploader < CarrierWave::Uploader::Base
+class PageImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
-
-  include CarrierWave::MimeTypes
-  process :set_content_type
+  storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -33,7 +26,6 @@ class ShowGuestImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process :resize_to_limit => [250, 0]
   #
   # def scale(width, height)
   #   # do something
@@ -43,7 +35,6 @@ class ShowGuestImageUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :scale => [50, 50]
   # end
-
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
