0e83d53434a4010c756e06b96553f6aad3b9b65b
Cavin Stone <cstone2003@hotmail.com>
2014-01-11 00:32:11 -0600

    Add ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Kevinandashley::Application.routes.draw do
 
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
+  ActiveAdmin.routes(self)
 
   get "photos/index"
 
